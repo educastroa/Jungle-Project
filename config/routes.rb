7245e18dc148ed725fb6cef2ef120740051bd327
educastroa <eduardocastro.a@hotmail.com>
2022-07-04 23:58:48 +0000

    complete user authentication features

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,17 @@ Rails.application.routes.draw do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
     resources :categories, only: [:index, :new, :create]
+    
   end
+
+
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  get '/logout' => 'session#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
